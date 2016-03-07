19eddaa31059f82837fb8d2b9cfeb58218fd4c4b
Thomas Visentin <tho.visentin@gmail.com>
2016-03-07 18:49:18 +0100

    Issue #14: backend Format validation

--- app/models/lead.rb
+++ app/models/lead.rb
@@ -1,4 +1,5 @@
 class Lead < ActiveRecord::Base
   validates :mail, presence: true
   validates :mail, uniqueness: true
+  validates_format_of :mail, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
 end
