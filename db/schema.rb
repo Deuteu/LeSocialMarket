765a1bb31b7318309f306ab7b29fb9e13b01f32e
Thomas Visentin <tho.visentin@gmail.com>
2016-03-07 18:40:31 +0100

    Config psql.

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160223175051) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "leads", force: :cascade do |t|
     t.string   "mail"
     t.datetime "created_at", null: false
