b45a120443743c144ad2c8f2d966b1c8982c18db
Thomas Visentin <tho.visentin@gmail.com>
2016-03-10 10:05:52 +0100

    Modif sur la forme

--- app/assets/javascripts/landing/cbpAnimatedHeader.js
+++ app/assets/javascripts/landing/cbpAnimatedHeader.js
@@ -4,7 +4,7 @@
  *
  * Licensed under the MIT license.
  * http://www.opensource.org/licenses/mit-license.php
- * 
+ *
  * Copyright 2013, Codrops
  * http://www.codrops.com
  */
@@ -35,10 +35,6 @@ var cbpAnimatedHeader = (function() {
 			classie.remove( header, 'navbar-shrink' );
 		}
 		didScroll = false;
-	}
-
-	function scrollY() {
-		return window.pageYOffset || docElem.scrollTop;
 	}
 
 	init();
